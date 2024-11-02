78d32f9f2edeee2b3b9eb8741a645d610883cc58
Fabian <fabianmtovar26@gmail.com>
2024-11-02 14:57:13 -0400

    carga de contenido dinamica agregada

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Pokedex from './pokedex.jsx'
 
 function App() {
 
   return (
-    <>
-      Hola mundo
-    </>
+    <Pokedex end={20}></Pokedex>
   )
 }
 
--- src/pokedex.jsx
+++ src/pokedex.jsx
@@ -1,16 +1,39 @@
-import { useState } from "react";
 import { Pokemon } from "./pokemon";
+import { useState, useEffect } from "react";
 
-export function Pokedex({start, end}) {
+const Pokedex = ({start, end}) => {
 
-  const _start = start || 0;
-  const pokemons = [];
-  for (let i = _start; i <= end; i++) {
-    pokemons.push(<Pokemon n={i}></Pokemon>);
-  }
+
+  const [pokemons, setPokemons] = useState([]);
+  let loading = false;
+  useEffect(() => {
+    const _start = start || 1;
+    const inicial_pokemons = [];
+    for (let i = _start; i <= end; i++) {
+      inicial_pokemons.push(<Pokemon key={i} n={i}></Pokemon>);
+    }
+    setPokemons(inicial_pokemons);
+    window.addEventListener("scroll", () => {
+      if (loading) return;
+      loading = true;
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
+        setPokemons((pokemons) => {
+          const new_pokemons = [...pokemons];
+          for (let i = 0; i < 20; i++) {
+            new_pokemons.push(<Pokemon key={new_pokemons.length + 1} n={new_pokemons.length + 1}></Pokemon>);
+          } 
+          return new_pokemons;
+        });
+      }
+      loading = false;
+    });
+  }, []);
+
   return (
     <div className="ft-pokedex">
       {pokemons}
     </div>
   );
 }
+
+export default Pokedex;
